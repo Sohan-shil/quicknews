4b5d709f593a58de5e52d8b454e34a9cac088e34
Sohan-shil <139703059+Sohan-shil@users.noreply.github.com>
2023-10-13 01:11:59 +0530

    done

--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
 import PropTypes from "prop-types";
+// import InfiniteScroll from 'react-infinite-scroll-component';
 
 const News = (props)=> {
   
@@ -14,12 +15,9 @@ const News = (props)=> {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
 
-    document.title = `${capitalizeFirstLetter(props.category)} - NewsMonkey`;
-  
-
   const updateNews = async ()=> {
     props.setProgress(20);
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page+1}&pageSize=${props.pageSize}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
     setLoading(true);
     props.setProgress(50);
     let data = await fetch(url);
@@ -31,8 +29,19 @@ const News = (props)=> {
     props.setProgress(100);
   }
 
+  // const fetchData = async () => {
+  //   const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page+1}&pageSize=${props.pageSize}`;
+  //   setPage(page+1) 
+  //   let data = await fetch(url);
+  //   let parsedData = await data.json()
+  //   setArticles(articles.concat(parsedData.articles))
+  //   setTotalResults(parsedData.totalResults)
+  // }
+
+
   useEffect(() => {
     updateNews()
+    document.title = `${capitalizeFirstLetter(props.category)} - NewsMonkey`;
     // eslint-disable-next-line 
   }, [])
   
@@ -52,9 +61,15 @@ const News = (props)=> {
       <div className="container my-4">
         <h1 className="text-center" style={{marginTop: '70px', marginBottom:'30px'}}>Top Headlines- {capitalizeFirstLetter(props.category)} </h1>
         {loading && <Spinner />}
+
+        {/* <InfiniteScroll
+          dataLength={articles.length} 
+          next={fetchData}
+          hasMore={true}
+          loader={<Spinner/>}> */}
+
         <div className="row">
-          {!loading &&
-            articles.map((element) => {
+          {articles.map((element) => {
               return (
                 <div className="col-md-4" key={element.url}>
                   <NewsItem
@@ -70,6 +85,9 @@ const News = (props)=> {
               );
             })}
         </div>
+
+        {/* </InfiniteScroll> */}
+
         <div className="container d-flex justify-content-between">
           <button disabled={page<=1} type="button" className="btn btn-success" onClick={handlePrevClick}>&larr; Previous</button>
     
